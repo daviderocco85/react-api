6ed0e483e06bbe42ae8974eff2daa7c121093209
daviderocco85 <roccodavide85@gmail.com>
2026-07-28 01:04:28 +0200

    feat: update FeatureActressesCard to improve layout and awards formatting

--- src/components/FeatureActressesCard.jsx
+++ src/components/FeatureActressesCard.jsx
@@ -5,14 +5,19 @@ export const FeatureActressesCard = props => {
         <>
             <div className="feature-card">
                 <div className="pic-actresses">
+
                     <img src={props.featureActresses.image} alt={props.featureActresses.name} />
                 </div>
                 <div className="bio">
                     <h2>{props.featureActresses.name}</h2>
-                    <p>{props.featureActresses.birth_year}</p>
-                    <p>{props.featureActresses.nationality}</p>
-                    <p>{props.featureActresses.biography}</p>
-                    <p>{props.featureActresses.awards}</p>
+                    <p>Anno di nascita - <span>{props.featureActresses.birth_year}</span></p>
+                    <p>Nazionalità - <span>{props.featureActresses.nationality}</span></p>
+                    <p>Bio - <span>{props.featureActresses.biography}</span></p>
+                    <p>Premi - <span>{props.featureActresses.awards
+                        .map(a => a.replace(/Awards?/i, '').trim())
+                        .join(' ')}
+                    </span>
+                    </p>
                 </div>
             </div>
         </>

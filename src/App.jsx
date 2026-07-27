4a15e01e1984f7a3787db34f68674197daa830de
daviderocco85 <roccodavide85@gmail.com>
2026-07-27 21:08:17 +0200

    feat: fetch actresses data using axios and log response to console

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const API_URL_ACTRESSES = 'https://lanciweb.github.io/demo/api/actresses/';
 
 export const App = () => {
+  const [feature, setFeature] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_URL_ACTRESSES)
+      .then(res => console.log(res.data));
+  }, []);
+
 
   return (
     <>
       Work in progress
     </>
-  )
-}
+  );
+};
 
 

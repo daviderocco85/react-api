261d613c21779abbad9ee9d17a980162b332738e
daviderocco85 <roccodavide85@gmail.com>
2026-07-27 23:02:28 +0200

    feat: add initial structure for FeatureActressesCard component

--- src/components/FeatureActressesCard.jsx
+++ src/components/FeatureActressesCard.jsx
@@ -1,9 +1,20 @@
 import './FeatureActressesCard.css';
 
-export const FeatureActressesCard = () => {
+export const FeatureActressesCard = props => {
     return (
         <>
-            Works
+            <div className="feature-card">
+                <div className="pic-actresses">
+                    <img src={props.featureActresses.image} alt={props.featureActresses.name} />
+                </div>
+                <div className="bio">
+                    <h2>{props.featureActresses.name}</h2>
+                    <p>{props.featureActresses.birth_year}</p>
+                    <p>{props.featureActresses.nationality}</p>
+                    <p>{props.featureActresses.biography}</p>
+                    <p>{props.featureActresses.awards}</p>
+                </div>
+            </div>
         </>
     );
 };

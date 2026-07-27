3cad72840768d88f3a298624037a06e30fb70494
daviderocco85 <roccodavide85@gmail.com>
2026-07-27 23:05:40 +0200

    feat: update App.jsx to fetch actresses API and render FeatureActressesCard list

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { useState, useEffect } from 'react';
+import { FeatureActressesCard } from './components/FeatureActressesCard.jsx';
 import axios from 'axios';
 import './App.css';
 
 const API_URL_ACTRESSES = 'https://lanciweb.github.io/demo/api/actresses/';
 
 export const App = () => {
-  const [feature, setFeature] = useState([]);
+  const [featureActresses, setFeatureActresses] = useState([]);
 
   useEffect(() => {
     axios.get(API_URL_ACTRESSES)
-      .then(res => console.log(res.data));
+      .then(res => {
+        console.log(res.data);
+        setFeatureActresses(res.data)
+      });
+
   }, []);
 
 
   return (
     <>
-      Work in progress
+      <h1>Attrici Famose</h1>
+      <div className="feature-actresses">
+        {featureActresses.map(feature => (
+          <FeatureActressesCard
+            key={feature.id}
+            featureActresses={feature}
+          />
+        ))}
+      </div>
     </>
   );
 };

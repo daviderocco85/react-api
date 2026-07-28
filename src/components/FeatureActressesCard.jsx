43d31c4e32979ea54641c3846f5f3a122712b9a4
daviderocco85 <roccodavide85@gmail.com>
2026-07-28 13:25:35 +0200

    feat: update FeatureActressesCard text labels to English

--- src/components/FeatureActressesCard.jsx
+++ src/components/FeatureActressesCard.jsx
@@ -1,19 +1,20 @@
 import './FeatureActressesCard.css';
 
 export const FeatureActressesCard = props => {
+
+
     return (
         <>
             <div className="feature-card">
                 <div className="pic-actresses">
-
                     <img src={props.featureActresses.image} alt={props.featureActresses.name} />
                 </div>
                 <div className="bio">
                     <h2>{props.featureActresses.name}</h2>
-                    <p>Anno di nascita - <span>{props.featureActresses.birth_year}</span></p>
-                    <p>Nazionalità - <span>{props.featureActresses.nationality}</span></p>
+                    <p>Birth year - <span>{props.featureActresses.birth_year}</span></p>
+                    <p>Nationality - <span>{props.featureActresses.nationality}</span></p>
                     <p>Bio - <span>{props.featureActresses.biography}</span></p>
-                    <p>Premi - <span>{props.featureActresses.awards
+                    <p>Awards - <span>{props.featureActresses.awards
                         .map(a => a.replace(/Awards?/i, '').trim())
                         .join(' ')}
                     </span>

19ea6bf840dd84c1a2fc93391c4f41695dc4e7d6
daviderocco85 <roccodavide85@gmail.com>
2026-07-28 13:18:28 +0200

    feat(pagination): show one actress card per page with navigation controls

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,43 @@ const API_URL_ACTRESSES = 'https://lanciweb.github.io/demo/api/actresses/';
 
 export const App = () => {
   const [featureActresses, setFeatureActresses] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    axios.get(API_URL_ACTRESSES)
+    axios.get(API_URL_ACTRESSES, { params: { page } })
       .then(res => {
         console.log(res.data);
         setFeatureActresses(res.data)
+      })
+      .catch(err => {
+        console.log(err);
       });
 
-  }, []);
+  }, [page]);
 
 
   return (
     <>
-      <h1>Attrici Famose</h1>
+      <h1>Famous Actresses</h1>
+      <div className='navigation'>
+        <button
+          onClick={() => setPage
+            (p => Math.max(1, p - 1)
+            )}>
+          Prev
+        </button>
+        <span className="page-number">{page}</span>
+        <button
+          onClick={() => setPage
+            (p => Math.min(featureActresses.length, p + 1)
+            )}>
+          Next
+        </button>
+      </div>
       <div className="feature-actresses">
-        {featureActresses.map(feature => (
-          <FeatureActressesCard
-            key={feature.id}
-            featureActresses={feature}
-          />
-        ))}
+        {featureActresses.length > 0 && (
+          <FeatureActressesCard featureActresses={featureActresses[page - 1]} />
+        )}
       </div>
     </>
   );
